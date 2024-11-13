2c61ddec23abaea849b1c8907681fe8841faf231
leif <leif@elliottnet.com>
2024-11-13 12:58:32 +0800

    configuration for wasm and nodepolyfills to work with vite (by extension sveltekit). 3 plugins relied on by meshsdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
+	plugins: [
+		sveltekit(),
+		wasm(),
+		topLevelAwait(),
+		nodePolyfills({
+			globals: { Buffer: true, global: true },
+			protocolImports: true
+		})
+	],
+	define: {
+		global: 'globalThis'
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
